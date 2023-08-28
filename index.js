b06b0a5fac2330dff42ec5809882d13bd818e8cf
sunilbastola <41153362+sunilbastola@users.noreply.github.com>
2023-08-28 11:24:33 +0545

    Add while Loop

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ output = [];
 var count = 1;
 
 function fizzBuzz(){
-
-    var number = output.push(count);
-    
-
-    if (number % 3 === 0 && number % 5 === 0) {
-        output.pop(number);
-        output.push("FizzBuzz");
+    while (count <= 100){
+        var number = output.push(count);
+        
+        if (number % 3 === 0 && number % 5 === 0) {
+            output.pop(number);
+            output.push("FizzBuzz");
+        }
+        
+        else if (number % 3 === 0 ){
+            output.pop(number);
+            output.push("Fizz");
+        }
+        else if (number % 5 === 0 ){
+            output.pop(number);
+            output.push("Buzz");
+        }
+        
+        
+        count++;
     }
-    
-    else if (number % 3 === 0 ){
-        output.pop(number);
-        output.push("Fizz");
-    }
-    else if (number % 5 === 0 ){
-        output.pop(number);
-        output.push("Buzz");
-    }
-    
     console.log(output);
-    count++;
 }
 fizzBuzz();
